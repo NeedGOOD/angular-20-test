d281500bfb1054066ab93f3f3264513976394f5a
NeedGOOD <vladyakusevich@gmail.com>
2025-08-30 16:53:00 +0300

    feat: start learn about components

---
+++ src/app/components/admin/admin.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+export class Admin {
+  firstName = 'Vlad';
+  siteName = 'Angular 20 test';
+  rollNo = 111;
+}
