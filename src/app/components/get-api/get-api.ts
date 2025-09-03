e7e4265cbe1edd8978b77d0fa21609d0f92c6588
NeedGOOD <vladyakusevich@gmail.com>
2025-09-03 23:09:47 +0300

    feat: GET API call integration

---
+++ src/app/components/get-api/get-api.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-api',
+  imports: [],
+  templateUrl: './get-api.html',
+  styleUrl: './get-api.css'
+})
+export class GetApi implements OnInit {
+  http = inject(HttpClient);
+  userList: any[] = [];
+  todoList: any[] = [];
+
+  ngOnInit(): void {
+    // debugger;
+    this.getUsers();
+    this.getTodoItems();
+  }
+
+  getUsers() {
+    // debugger;
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res: any) => {
+      // debugger;
+      this.userList = res;
+    });
+  }
+
+  getTodoItems() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe((res: any) => {
+      this.todoList = res;
+    });
+  }
+}
