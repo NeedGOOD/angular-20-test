ace3be55867c415faa8ac199016ad876b0e277ef
NeedGOOD <vladyakusevich@gmail.com>
2025-09-06 23:08:49 +0300

    feat: put & delete api in angular

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -43,4 +43,45 @@ export class User implements OnInit {
       },
     });
   }
+
+  onEdit(item: any) {
+    this.userObj = item;
+  }
+
+  onUpdateUser() {
+    // this.userObj.createdDate = new Date();
+    this.http.put(`https://api.freeprojectapi.com/api/GoalTracker/updateUser?id=` + this.userObj.userId, this.userObj).subscribe({
+      next: () => {
+        alert('User updated success');
+        this.getUsers();
+      },
+      error: (error) => {
+        alert('Error - ' + error.error);
+      }
+    });
+  }
+
+  onDelete(id: number) {
+    const isDelete = confirm("Are you sure want to delete?");
+    if (isDelete)
+      this.http.delete(`https://api.freeprojectapi.com/api/GoalTracker/deleteUserById?id=` + id).subscribe({
+        next: () => {
+          alert('User delete success');
+          this.getUsers();
+        },
+        error: (error) => {
+          alert('Error - ' + error.error);
+        }
+      });
+  }
+
+  onReset() {
+    this.userObj = {
+      "userId": 0,
+      "emailId": "",
+      "password": "",
+      "fullName": "",
+      "mobileNo": "",
+    }
+  }
 }
